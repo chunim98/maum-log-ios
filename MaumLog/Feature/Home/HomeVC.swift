68f0a107fcf8ed8a9cf7c6080f61c092e3059ba9
JU98965 <jjingeo1230@naver.com>
2025-03-11 23:21:18 +0900

    refactor: Home 모듈 Reactive 인터페이스 도입 완료 #10

--- MaumLog/Feature/Home/HomeVC.swift
+++ MaumLog/Feature/Home/HomeVC.swift
@@ -9,7 +9,6 @@ import UIKit
 
 import RxSwift
 import RxCocoa
-import RxDataSources
 import SnapKit
 
 final class HomeVC: UIViewController {
@@ -21,163 +20,228 @@ final class HomeVC: UIViewController {
     
     // MARK: Components
     
-    let titleLabel = {
+    private let titleLabel = {
         let label = UILabel()
-        label.text = String(localized: "대시보드")
         label.font = .boldSystemFont(ofSize: 24)
         label.textColor = .chuBlack
+        label.text = "대시보드"
         return label
     }()
     
-    let goSettingsBarButton = {
-        let image = UIImage(named: "gear")?.resizeImage(newWidth: 24)
-        let barButton = UIBarButtonItem(image: image, style: .plain, target: .none, action: .none)
+    private let settingsBarButton = {
+        let barButton = UIBarButtonItem(
+            image: UIImage(named: "gear")?.resizeImage(newWidth: 24),
+            style: .plain,
+            target: .none,
+            action: .none
+        )
         barButton.tintColor = .chuBlack
         return barButton
     }()
     
-    let scrollview = {
+    fileprivate let scrollView = {
+        let refresh = UIRefreshControl()
+        refresh.attributedTitle = NSAttributedString(
+            string: "당겨서 새로고침",
+            attributes: [.foregroundColor : UIColor.gray]
+        )
         let view = UIScrollView()
-        view.refreshControl = UIRefreshControl()
-        view.refreshControl?.attributedTitle = NSAttributedString(
-            string: String(localized: "당겨서 새로고침"),
-            attributes: [NSAttributedString.Key.foregroundColor : UIColor.gray])
-        view.clipsToBounds = false
         view.backgroundColor = .chuIvory
+        view.refreshControl = refresh
+        view.clipsToBounds = false
         return view
     }()
     
-    let contentView = {
+    private let contentView = {
         let view = UIView()
-        view.clipsToBounds = false
         view.backgroundColor = .chuIvory
+        view.clipsToBounds = false
         return view
     }()
     
-    let mainVStack = {
+    private let mainVStack = {
         let sv = UIStackView()
         sv.axis = .vertical
         sv.spacing = 15
         return sv
     }()
     
-    let symptomView = SymptomSectionView()
-    let medicineView = MedicineSectionView()
-    let calendarView = CalendarSectionView()
+    fileprivate let symptomView = SymptomSectionView()
+    fileprivate let medicineView = MedicineSectionView()
+    private let calendarView = CalendarSectionView()
     
-    // MARK: - Life Cycle
+    // MARK: Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .chuIvory
-        
         setNavigationBar(
             leftBarButtonItems: [UIBarButtonItem(customView: titleLabel)],
-            rightBarButtonItems: [goSettingsBarButton]
+            rightBarButtonItems: [settingsBarButton]
         )
         setAutoLayout()
         setBinding()
     }
     
-    // MARK: - Layout
+    // MARK: Layout
+    
     func setAutoLayout() {
-        view.addSubview(scrollview)
-        scrollview.addSubview(contentView)
+        view.addSubview(scrollView)
+        scrollView.addSubview(contentView)
         contentView.addSubview(mainVStack)
         mainVStack.addArrangedSubview(symptomView)
         mainVStack.addArrangedSubview(medicineView)
         mainVStack.addArrangedSubview(calendarView)
         
-        scrollview.snp.makeConstraints { $0.edges.equalTo(view.safeAreaLayoutGuide) }
+        scrollView.snp.makeConstraints {
+            $0.edges.equalTo(view.safeAreaLayoutGuide)
+        }
         contentView.snp.makeConstraints {
             $0.verticalEdges.equalToSuperview()
             $0.width.equalToSuperview()
         }
         mainVStack.snp.makeConstraints {
-            $0.edges.equalToSuperview().inset(UIEdgeInsets(top: 20, left: 15, bottom: 50, right: 15))
+            let inset = UIEdgeInsets(top: 20, left: 15, bottom: 50, right: 15)
+            $0.edges.equalToSuperview().inset(inset)
         }
     }
     
-    // MARK: - Binding
+    // MARK: Binding
+    
     func setBinding() {
-        // 스크롤 뷰를 잡아당겼을 때 리프레쉬가 필요하다는 메시지 전달
-        let startRefreshing = scrollview.refreshControl?
-            .rx.controlEvent(.valueChanged).asObservable()
-            .do(onNext: { [weak self] _ in
-                self?.symptomView.rx.reloadBinder.onNext(())
-                self?.medicineView.rx.reloadBinder.onNext(())
-                self?.calendarView.rx.reloadBinder.onNext(())
-            }) ?? .empty() // nil이면 아무것도 방출하지 않고 스트림 종료
+        let refreshEvnet = scrollView.refreshControl?
+            .rx.controlEvent(.valueChanged)
+            .asObservable() ?? .empty()
 
         let input = HomeVM.Input(
-            tappedGoSettingsButton: goSettingsBarButton.rx.tap.asObservable(),
-            startRefreshing: startRefreshing,
-            goAddSymptom: symptomView.rx.addButtonTapEvent,
-            presentRemoveSymptomAlert: symptomView.rx.itemToRemove,
-            goAddMedicine: medicineView.rx.addButtonTapEvent,
-            presentRemoveMedicineAlert: medicineView.rx.itemToRemove
+            refreshEvnet: refreshEvnet,
+            pushSettingsEvent: settingsBarButton.rx.tap.asObservable(),
+            pushAddSymptomEvent: symptomView.rx.addButtonTapEvent,
+            pushAddMedicineEvent: medicineView.rx.addButtonTapEvent,
+            symptomToRemove: symptomView.rx.itemToRemove,
+            medicineToRemove: medicineView.rx.itemToRemove
         )
-        
-        // MARK: - Output
         let output = homeVM.transform(input)
         
-        // 설정 화면 이동
-        output.goSettings
-            .bind(with: self) { owner, _ in
-                let vc = SettingsVC()
-                vc.hidesBottomBarWhenPushed = true
-                owner.navigationController?.pushViewController(vc, animated: true)
-            }
+        // 리프레시 컨트롤의 데이터 갱신 요청
+        output.refreshEvnet
+            .bind(
+                to: symptomView.rx.reloadBinder,
+                medicineView.rx.reloadBinder,
+                calendarView.rx.reloadBinder
+            )
             .disposed(by: bag)
         
-        // 스크롤 뷰 리프레쉬 종료
-        output.endRefreshing
-            .bind(with: self) { owner, _ in
-                owner.scrollview.refreshControl?.endRefreshing()
-                HapticManager.shared.occurSuccess()
-            }
+        // 리프레쉬 종료
+        output.refreshEvnet
+            .bind(to: self.rx.endRefreshBinder)
+            .disposed(by: bag)
+        
+        // 설정 화면 이동
+        output.pushSettingsEvent
+            .bind(to: self.rx.pushSettingsBinder)
             .disposed(by: bag)
         
         // 증상 추가 모달 띄우기
-        output.goAddSymptom
-            .bind(with: self) { owner, _ in
-                let vc = AddSymptomVC()
-                let fraction = UISheetPresentationController.Detent.custom { _ in owner.view.frame.height * 0.5 }
-                if let sheet = vc.sheetPresentationController {
-                    sheet.detents = [fraction]
-                    sheet.preferredCornerRadius = .chuRadius // 모달 모서리 굴곡
-                }
-                // 창을 닫을 때 리로드 메시지 전송
-                vc.dismissTask = { owner.symptomView.rx.reloadBinder.onNext(()) }
-                owner.present(vc, animated: true)
-            }
-            .disposed(by: bag)
-        
-        // 증상, 약물 삭제 확인 얼럿 띄우기
-        output.presentRemoveAlert
-            .bind(with: self) { owner, item in
-                owner.presentRemoveAlert(item: item)
-            }
+        output.pushAddSymptomEvent
+            .bind(to: self.rx.pushAddSymptomBinder)
             .disposed(by: bag)
         
         // 약물 추가 모달 띄우기
-        output.goAddMedicine
-            .bind(with: self) { owner, _ in
-                let vc = AddMedicineVC()
-                let fraction = UISheetPresentationController.Detent.custom { _ in owner.view.frame.height * 0.3 }
-                if let sheet = vc.sheetPresentationController {
-                    sheet.detents = [fraction]
-                    sheet.preferredCornerRadius = .chuRadius // 모달 모서리 굴곡
-                }
-                // 창을 닫을 때 리로드 메시지 전송
-                vc.dismissTask = { owner.medicineView.rx.reloadBinder.onNext(()) }
-                owner.present(vc, animated: true)
-            }
+        output.pushAddMedicineEvent
+            .bind(to: self.rx.pushAddMedicineBinder)
+            .disposed(by: bag)
+        
+        // 증상, 약물 삭제 확인 얼럿 띄우기
+        output.itemToRemove
+            .bind(to: self.rx.presentRemoveAlertBinder)
             .disposed(by: bag)
     }
 }
 
-#Preview {
-    TabBarVC()
+#Preview { TabBarVC() }
+
+// MARK: - Reactive
+
+extension Reactive where Base: HomeVC {
+    
+    fileprivate var endRefreshBinder: Binder<Void> {
+        Binder(base) { base, _ in
+            base.scrollView.refreshControl?.endRefreshing()
+            HapticManager.shared.occurSuccess()
+        }
+    }
+    
+    fileprivate var pushSettingsBinder: Binder<Void> {
+        Binder(base) { base, _ in
+            let vc = SettingsVC()
+            vc.hidesBottomBarWhenPushed = true
+            base.navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+    
+    fileprivate var pushAddSymptomBinder: Binder<Void> {
+        Binder(base) { base, _ in
+            let fraction = UISheetPresentationController.Detent.custom { _ in
+                base.view.frame.height * 0.5
+            }
+            
+            let vc = AddSymptomVC()
+            vc.sheetPresentationController?.detents = [fraction]
+            vc.sheetPresentationController?.preferredCornerRadius = .chuRadius // 모달 모서리 굴곡
+            vc.dismissTask = { base.symptomView.rx.reloadBinder.onNext(()) } // 창 닫을 때, 새로고침
+            base.present(vc, animated: true)
+        }
+    }
+    
+    fileprivate var pushAddMedicineBinder: Binder<Void> {
+        Binder(base) { base, _ in
+            let fraction = UISheetPresentationController.Detent.custom { _ in
+                base.view.frame.height * 0.3
+            }
+            let vc = AddMedicineVC()
+            vc.sheetPresentationController?.detents = [fraction]
+            vc.sheetPresentationController?.preferredCornerRadius = .chuRadius // 모달 모서리 굴곡
+            vc.dismissTask = { base.medicineView.rx.reloadBinder.onNext(()) } // 창 닫을 때, 새로고침
+            base.present(vc, animated: true)
+        }
+    }
+    
+    fileprivate var presentRemoveAlertBinder: Binder<EditButtonCellModel> {
+        Binder(base) { $0.presentRemoveAlert(item: $1) }
+    }
+}
+
+// MARK: - Alerts
+
+extension HomeVC {
+    func presentRemoveAlert(item: any EditButtonCellModel) {
+        switch item {
+        case let item as SymptomData:
+            presentAlert(
+                title: "알림",
+                message: "\"\(item.name)\" 증상을 목록에서 삭제할까요?",
+                acceptTitle: "삭제",
+                acceptTask: {
+                    SymptomDataManager.shared.delete(target: item) // 등록한 증상 삭제
+                    self.symptomView.rx.reloadBinder.onNext(()) // 리로드 메시지 전송
+                }
+            )
+            
+        case let item as MedicineData:
+            presentAlert(
+                title: "알림",
+                message: "\"\(item.name)\" 을 목록에서 삭제할까요?",
+                acceptTitle: "삭제",
+                acceptTask: {
+                    MedicineDataManager.shared.delete(target: item) // 등록한 약물 삭제
+                    self.medicineView.rx.reloadBinder.onNext(()) // 리로드 이벤트 전송
+                }
+            )
+            
+        default:
+            print(#function, "예외 발생")
+        }
+    }
 }
 
 
